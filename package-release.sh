457bac05ad3471ccf5c13afa97d5c2fae15923e4
EBK21 <chkd13303@gmail.com>
2023-02-08 03:01:45 +0800

    use our toolchain

--- package-release.sh
+++ package-release.sh
@@ -83,6 +83,9 @@ function package {
   rm -R "dxvk-$DXVK_VERSION"
 }
 
+curl -L "https://github.com/eebssk1/mingw-gcc-build/releases/download/f6e9e326/mingw-gcc.tgz" | tar -zxf -
+export PATH=$PWD/mingw-gcc/i686-w64-mingw32/bin:$PWD/mingw-gcc/x86_64-w64-mingw32/bin:$PATH
+
 build_arch 64
 build_arch 32
 
